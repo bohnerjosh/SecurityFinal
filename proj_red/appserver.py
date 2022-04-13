4707d0a06beae5064ea1f045d14bb9295de993e0
bohnerjosh <squash0002@gmail.com>
2022-04-12 20:13:22 -0500

    added cow route

--- proj_red/appserver.py
+++ proj_red/appserver.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify, abort
-from flask_sqlalchemy import SQLAlchemy
-import os 
 
 app = Flask(__name__)
-app.secret_key = b'ABCDEFG#(*JXKNCW:Q"vwads6it7y'
+app.secret_key = b'ahcqweiu55pjds6it7y'
 
-sqlite_uri = 'sqlite:///' + os.path.abspath(os.path.curdir) + '/test.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = sqlite_uri
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-from models import User
-
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Results of GET /'
+    return render_template("website.html")
+
+@app.route("/cow")
+def cow():
+    return render_template("cow.html")
