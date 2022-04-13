c70791ddecbaba813207aa407c9fb28cddfbeb18
bohnerjosh <squash0002@gmail.com>
2022-04-13 09:49:06 -0500

    added route functionality for cowsay

--- proj_red/appserver.py
+++ proj_red/appserver.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request, session, redirect, url_for, jsonify, abort
 
+from jinja2 import Environment, PackageLoader, select_autoescape
+env = Environment(
+   keep_trailing_newline=True 
+)
+import os
 app = Flask(__name__)
 app.secret_key = b'ahcqweiu55pjds6it7y'
 
@@ -7,6 +12,20 @@ app.secret_key = b'ahcqweiu55pjds6it7y'
 def index():
     return render_template("website.html")
 
-@app.route("/cow")
+@app.route("/cow", methods=["GET"])
 def cow():
     return render_template("cow.html")
+
+@app.route("/cow", methods=["POST"])
+def process_text():
+    cowtext = ""
+    user_input = request.form["input_text"]
+    os.system(f"cowsay {user_input} > out.txt")
+    with open("out.txt", "r") as f:
+        cowtext = f.read()
+    os.remove("out.txt")
+    return render_template("cow.html", cowtext = cowtext)
+
+@app.route("/sent")
+def message_sent():
+    return render_template("sent.html")
